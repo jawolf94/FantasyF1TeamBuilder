a59510d9fd64baa1c5e7e7404868ab9791c75c9d
jawolf94 <jawolf17@gmail.com>
2024-03-20 16:56:55 -0400

    Renamed analytics project folder to match project name

---
+++ Analytics/Predictions/IPrediction.cs
@@ -0,0 +1,19 @@
+﻿using Results.Data;
+
+namespace Analytics.Predictions;
+
+/// <summary>
+/// A prediction about fantasy data.
+/// </summary>
+public interface IPrediction
+{
+	/// <summary>
+	/// Returns true if this prediction applies.
+	/// </summary>
+	public bool CanApply(RaceResults teamComponent);
+
+	/// <summary>
+	/// Predicted number of points gained or lost.
+	/// </summary>
+	public double PredictPoints(RaceResults teamComponent);
+}
