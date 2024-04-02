a6f8962d798044b27ce8373d0efc202a80dfad75
jawolf94 <jawolf17@gmail.com>
2024-04-01 20:34:25 -0400

    Fantasy teams can now track the total number of transfers between race weekends. Points are deducted from the total when excess transfers are made.

---
+++ Fantasy/Rules/TransferAllowances.cs
@@ -0,0 +1,28 @@
+﻿namespace Fantasy.Rules;
+
+public static class TransferAllowances
+{
+	/// <summary>
+	/// Number of free transfers allowed per race week.
+	/// </summary>
+	public const int FreeTransferAllowance = 2;
+
+	/// <summary>
+	/// Number of points lost per transfer over the allowance.
+	/// </summary>
+	public const double PointsPerAdditionalTransfer = 10;
+
+	/// <summary>
+	/// Calculates the number of points lost given the number of transfers made.
+	/// </summary>
+	public static double CalculateCostOfTransfers(int numberOfTransfers) 
+	{
+		if (numberOfTransfers <= FreeTransferAllowance) 
+		{
+			return 0;
+		}
+
+		var numberOfExcessiveTransfers = numberOfTransfers - FreeTransferAllowance;
+		return numberOfExcessiveTransfers * PointsPerAdditionalTransfer;
+	}
+}
